fc4c32e50485a05bfd6cb61b9f4bd150c1767496
Tom Hewitt <tommhewitt@gmail.com>
2019-01-30 22:04:42 +0000

    Add Imports

---
+++ Assets/Imports/Explosion/DestroyAfterLifetime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterLifetime : MonoBehaviour 
+{
+    public float lifetime;
+
+	void Start()
+	{
+		Destroy (gameObject, lifetime);
+	}
+}
---
+++ Assets/Imports/Explosion/TargetExplosion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Unsure if this works, but plays a random sound out of a possible four sounds
+/// </summary>
+public class TargetExplosion : MonoBehaviour 
+{
+	public AudioSource[] clips;
+    public AudioSource clip;
+
+	void Start()
+	{
+        clip = clips[Random.Range(0, 3)];
+        clip.Play();
+	}
+}
