3ccb9f8dbe2af35f23fc1ec6faf527027d4e632e
Tom Hewitt <tommhewitt@gmail.com>
2019-03-03 17:52:50 +0000

    Added Oculus Support

--- Assets/Scripts/Utilities/AudioManager.cs
+++ Assets/Scripts/Utilities/AudioManager.cs
@@ -82,7 +82,7 @@ public class AudioManager : MonoBehaviour
 
         if (s == null)
         {
-            print("Warning! Sound: '" + name + "'was not found.");
+            //print("Warning! Sound: '" + name + "'was not found.");
             return null;
         }
         return s;
